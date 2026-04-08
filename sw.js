ed627b7cb095410d475e9e126c0b76f66a0e5ce9
conceptideas0-droid <conceptideas0@gmail.com>
2026-04-08 12:45:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'inventory-webapp-v1';
+const APP_SHELL = [
+  '/controlcheck/WebApp.html',
+  '/controlcheck/AddItemsWeb.html',
+  '/controlcheck/MoveItemWeb.html',
+  '/controlcheck/SearchWeb.html',
+  '/controlcheck/DashboardWeb.html',
+  '/controlcheck/ReportGenerator.html',
+  '/controlcheck/QR/index.html',
+  '/controlcheck/manifest.json',
+  '/controlcheck/frontend/icons/icon-192.png',
+  '/controlcheck/frontend/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.pathname.startsWith('/api')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      });
+    })
+  );
+});
